0f9bbd32351b89659adb260a6fa0f0570f74ca00
Nilay-Kalaskar <spartankalaskar@gmail.com>
2024-01-10 10:32:50 +0530

    Nilay Project

---
+++ examportal/lib/data/ansdata.dart
@@ -0,0 +1,48 @@
+import 'package:examportal/model/questions.dart';
+// import 'package:examportal/qust.dart';
+
+var answer = [
+  Questions("Who developed Flutter ?",[
+  "facebook","adobe","google","microsoft"
+  ],
+"google",
+  ),
+  Questions("What is the capital of france ?",
+  ["Berlin", "Madrid", "Paris", "Rome"],
+   "Paris",
+  ),
+  Questions("Which planet is known as the Red Planet?",
+ ["Mars", "Venus", "Jupiter", "Saturn"],
+  "Mars",
+  ),
+  Questions("What is the largest mammal?",
+ ["Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
+ "Blue Whale",
+  ),
+  Questions("Which programming language is used to build Flutter applications?",
+ ["kotlin", "java", "swift", "dart"],
+ "dart",
+  ),
+  Questions("How many types of widgets are there in Flutter?",
+ ["two", "five", "eight", "four"],
+ "two",
+  ),
+  Questions("Access to a cloud database through Flutter is available through which service?",
+ [" SQLite", "NOSQL", "Firebase Database", "MYSQL"],
+ "Firebase Database",
+  ),
+  Questions("What element is used as an identifier for components when programming in Flutter?",
+ [" Widgets", "Keys", "Elements", "Serial"],
+ "Keys",
+  ),
+  Questions("Which function will return the widgets attached to the screen as a root of the widget tree to be rendered on screen?",
+ ["main()", "container()", "runApp()", "MaterialApp()"],
+ "runApp()",
+  ),
+  Questions("What is the key configuration file used when building a Flutter project?",
+ ["pubsec.yaml", "pubsec.xml", " config.html", " config.html"],
+ "pubsec.yaml",
+  
+  ),
+
+];
---
+++ examportal/lib/model/questions.dart
@@ -0,0 +1,9 @@
+class Questions {
+  Questions(this.text, this.answers, this.rightAnswers);
+
+  String? text;
+  List<String> answers;
+  String? correctedAnswer;
+
+  String rightAnswers;
+}
